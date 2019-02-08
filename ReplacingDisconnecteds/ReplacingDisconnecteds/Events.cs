1a691692280e6341d6449245d4ad5b8c32ae92fc
InsetJesux <insetjesux@gmail.com>
2019-02-08 15:20:32 +0100

    Change config names, nest plugin.dropitems and disable plugin check

--- ReplacingDisconnecteds/ReplacingDisconnecteds/Events.cs
+++ ReplacingDisconnecteds/ReplacingDisconnecteds/Events.cs
@@ -7,148 +7,153 @@ using System.IO;
 
 namespace ReplacingDisconnecteds
 {
-    class Events : IEventHandlerDisconnect, IEventHandlerCallCommand, IEventHandlerWaitingForPlayers
-    {
-        public readonly string directory = "ReplacingDisconnecteds";
+	class Events : IEventHandlerDisconnect, IEventHandlerCallCommand, IEventHandlerWaitingForPlayers
+	{
+		public readonly string directory = "ReplacingDisconnecteds";
 
-        private ReplacingDisconnecteds plugin;
+		private ReplacingDisconnecteds plugin;
 
-        public Events(ReplacingDisconnecteds plugin)
-        {
-            this.plugin = plugin;
-        }
+		public Events(ReplacingDisconnecteds plugin)
+		{
+			this.plugin = plugin;
+		}
 
-        public void OnDisconnect(DisconnectEvent ev)
-        {
-            List<Player> Players = PluginManager.Manager.Server.GetPlayers();
-            Player SpectatorPlayer = SearchSpectator(Players);
-            Player DisconnectedPlayer = SearchDisconnectedPlayer(Players);
-            
-            //Cannot find disconnected player
-            if (DisconnectedPlayer == null) return;
+		public void OnDisconnect(DisconnectEvent ev)
+		{
+			if (!plugin.AllowUserChoice && !plugin.DropItems && !plugin.ForceValue) return;
 
-            if (SpectatorPlayer == null && plugin.DropItems)
-            {
-                //Drop items
-                DropItems(DisconnectedPlayer);
-            }
-            else
-            {
-                //Replace player
-                ReplaceSpectator(SpectatorPlayer, DisconnectedPlayer);
-            }
-        }
+			List<Player> Players = PluginManager.Manager.Server.GetPlayers();
+			Player SpectatorPlayer = SearchSpectator(Players);
+			Player DisconnectedPlayer = SearchDisconnectedPlayer(Players);
+			
+			//Cannot find disconnected player
+			if (DisconnectedPlayer == null) return;
 
-        public void OnCallCommand(PlayerCallCommandEvent ev)
-        {
-            string command = ev.Command.ToLower();
+			if (SpectatorPlayer == null)
+			{
+				//Drop items
+				if (plugin.DropItems)
+				{
+					DropItems(DisconnectedPlayer);
+				}
+			}
+			else
+			{
+				//Replace player
+				ReplaceSpectator(SpectatorPlayer, DisconnectedPlayer);
+			}
+		}
 
-            switch (command)
-            {
-                case "sod enable":
-                    ev.ReturnMessage = EnablePlayerReplace(ev.Player);
-                    break;
-                case "sod disable":
-                    ev.ReturnMessage = DisablePlayerReplace(ev.Player);
-                    break;
-            }
-        }
+		public void OnCallCommand(PlayerCallCommandEvent ev)
+		{
+			string command = ev.Command.ToLower();
 
-        public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
-        {
-            plugin.RefreshConfig();
-        }
+			switch (command)
+			{
+				case "sod enable":
+					ev.ReturnMessage = EnablePlayerReplace(ev.Player);
+					break;
+				case "sod disable":
+					ev.ReturnMessage = DisablePlayerReplace(ev.Player);
+					break;
+			}
+		}
 
-        public void ReplaceSpectator(Player spectator, Player disconnected)
-        {
-            spectator.ChangeRole(disconnected.TeamRole.Role, false, false, false, false);
-            spectator.SetHealth(disconnected.GetHealth(), DamageType.WALL);
-            spectator.Teleport(disconnected.GetPosition(), true);
-            foreach (Item item in disconnected.GetInventory())
-            {
-                spectator.GiveItem(item.ItemType);
-            }
-            spectator.SetAmmo(AmmoType.DROPPED_5, disconnected.GetAmmo(AmmoType.DROPPED_5));
-            spectator.SetAmmo(AmmoType.DROPPED_7, disconnected.GetAmmo(AmmoType.DROPPED_7));
-            spectator.SetAmmo(AmmoType.DROPPED_9, disconnected.GetAmmo(AmmoType.DROPPED_9));
-        }
+		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+		{
+			plugin.RefreshConfig();
+		}
 
-        public void DropItems(Player disconnected)
-        {
-            foreach (Item item in disconnected.GetInventory())
-            {
-                PluginManager.Manager.Server.Map.SpawnItem(item.ItemType, disconnected.GetPosition(), new Vector(0, 0, 0));
-            }
-        }
+		public void ReplaceSpectator(Player spectator, Player disconnected)
+		{
+			spectator.ChangeRole(disconnected.TeamRole.Role, false, false, false, false);
+			spectator.SetHealth(disconnected.GetHealth(), DamageType.WALL);
+			spectator.Teleport(disconnected.GetPosition(), true);
+			foreach (Item item in disconnected.GetInventory())
+			{
+				spectator.GiveItem(item.ItemType);
+			}
+			spectator.SetAmmo(AmmoType.DROPPED_5, disconnected.GetAmmo(AmmoType.DROPPED_5));
+			spectator.SetAmmo(AmmoType.DROPPED_7, disconnected.GetAmmo(AmmoType.DROPPED_7));
+			spectator.SetAmmo(AmmoType.DROPPED_9, disconnected.GetAmmo(AmmoType.DROPPED_9));
+		}
 
-        public Player SearchDisconnectedPlayer(List<Player> PlayerList)
-        {
-            foreach (Player player in PlayerList)
-            {
-                if ((player.IpAddress == "" || player.IpAddress == null) && (player.TeamRole.Role != Role.SPECTATOR || player.TeamRole.Role != Role.TUTORIAL))
-                {
-                    return player;
-                }
-            }
-            return null;
-        }
+		public void DropItems(Player disconnected)
+		{
+			foreach (Item item in disconnected.GetInventory())
+			{
+				PluginManager.Manager.Server.Map.SpawnItem(item.ItemType, disconnected.GetPosition(), new Vector(0, 0, 0));
+			}
+		}
 
-        public Player SearchSpectator(List<Player> PlayerList)
-        {
-            foreach (Player player in PlayerList)
-            {
-                if (player.TeamRole.Role == Role.SPECTATOR)
-                {
-                    if (plugin.ForceReplace)
-                    {
-                        if (plugin.ReplaceValue == true)
-                        {
-                            return player;
-                        }
-                    }
-                    else
-                    {
-                        if (ReadUserConfig(player) == true)
-                        {
-                            return player;
-                        }
-                    }
-                }
-            }
-            return null;
-        }
+		public Player SearchDisconnectedPlayer(List<Player> PlayerList)
+		{
+			foreach (Player player in PlayerList)
+			{
+				if ((player.IpAddress == "" || player.IpAddress == null) && (player.TeamRole.Role != Role.SPECTATOR || player.TeamRole.Role != Role.TUTORIAL))
+				{
+					return player;
+				}
+			}
+			return null;
+		}
 
-        public bool ReadUserConfig(Player player)
-        {
-            //Default server config
-            if (!File.Exists($"{directory}/{player.SteamId}.txt"))
-            {
-                return plugin.DefaultSettings;
-            }
+		public Player SearchSpectator(List<Player> PlayerList)
+		{
+			foreach (Player player in PlayerList)
+			{
+				if (player.TeamRole.Role == Role.SPECTATOR)
+				{
+					if (!plugin.AllowUserChoice)
+					{
+						if (plugin.ForceValue == true)
+						{
+							return player;
+						}
+					}
+					else
+					{
+						if (ReadUserConfig(player) == true)
+						{
+							return player;
+						}
+					}
+				}
+			}
+			return null;
+		}
 
-            //Specific user config
-            if (File.ReadAllText($"{directory}/{player.SteamId}.txt") == "true")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+		public bool ReadUserConfig(Player player)
+		{
+			//Default server config
+			if (!File.Exists($"{directory}/{player.SteamId}.txt"))
+			{
+				return plugin.DefaultSettings;
+			}
 
-        public string EnablePlayerReplace(Player player)
-        {
-            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-            File.WriteAllText($"{directory}/{player.SteamId}.txt", "true");
-            return "Replacing disconnected player on";
-        }
+			//Specific user config
+			if (File.ReadAllText($"{directory}/{player.SteamId}.txt") == "true")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 
-        public string DisablePlayerReplace(Player player)
-        {
-            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-            File.WriteAllText($"{directory}/{player.SteamId}.txt", "false");
-            return "Replacing disconnected player off";
-        }
-    }
+		public string EnablePlayerReplace(Player player)
+		{
+			if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+			File.WriteAllText($"{directory}/{player.SteamId}.txt", "true");
+			return "Replacing disconnected player on";
+		}
+
+		public string DisablePlayerReplace(Player player)
+		{
+			if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+			File.WriteAllText($"{directory}/{player.SteamId}.txt", "false");
+			return "Replacing disconnected player off";
+		}
+	}
 }
--- ReplacingDisconnecteds/ReplacingDisconnecteds/ReplacingDisconnecteds.cs
+++ ReplacingDisconnecteds/ReplacingDisconnecteds/ReplacingDisconnecteds.cs
@@ -19,38 +19,38 @@ namespace ReplacingDisconnecteds
 
 	public class ReplacingDisconnecteds : Plugin
 	{
-        public bool ForceReplace { get; set; }
-        public bool ReplaceValue { get; set; }
-        public bool DefaultSettings { get; set; }
-        public bool DropItems { get; set; }
+		public bool AllowUserChoice { get; set; }
+		public bool ForceValue { get; set; }
+		public bool DefaultSettings { get; set; }
+		public bool DropItems { get; set; }
 
-        public override void OnDisable()
-        {
-            this.Info("ReplacingDisconnecteds hasn't loaded :'(");
-        }
+		public override void OnDisable()
+		{
+			this.Info("ReplacingDisconnecteds hasn't loaded :'(");
+		}
 
 		public override void OnEnable()
 		{
 			this.Info("ReplacingDisconnecteds has loaded <3");
-        }
+		}
 
-        public override void Register()
+		public override void Register()
 		{
-            //Events
-            this.AddEventHandlers(new Events(this));
-            //Configs
-            AddConfig(new ConfigSetting("sod_force_replace", false, SettingType.BOOL, true, "Force setting replace for all users"));
-            AddConfig(new ConfigSetting("sod_force_replace_value", false, SettingType.BOOL, true, "Force this value for all players, if sod_force_setting"));
-            AddConfig(new ConfigSetting("sod_dropitems", true, SettingType.BOOL, true, "Drop items if there are no spectators available"));
-            AddConfig(new ConfigSetting("sod_default_setting", true, SettingType.BOOL, true, "Default configuration for users who haven't changed it"));
-        }
+			//Events
+			this.AddEventHandlers(new Events(this));
+			//Configs
+			AddConfig(new ConfigSetting("sod_allow_user_choice", false, SettingType.BOOL, true, "Force setting replace for all users"));
+			AddConfig(new ConfigSetting("sod_force_value", false, SettingType.BOOL, true, "Force this value for all players, if sod_force_setting"));
+			AddConfig(new ConfigSetting("sod_dropitems", true, SettingType.BOOL, true, "Drop items if there are no spectators available"));
+			AddConfig(new ConfigSetting("sod_default_setting", true, SettingType.BOOL, true, "Default configuration for users who haven't changed it"));
+		}
 
-        public void RefreshConfig()
-        {
-            ForceReplace = GetConfigBool("sod_force_replace");
-            ReplaceValue = GetConfigBool("sod_force_replace_value");
-            DefaultSettings = GetConfigBool("sod_default_setting");
-            DropItems = GetConfigBool("sod_dropitems");
-        }
-    }
+		public void RefreshConfig()
+		{
+			AllowUserChoice = GetConfigBool("sod_allow_user_choice");
+			ForceValue = GetConfigBool("sod_force_value");
+			DefaultSettings = GetConfigBool("sod_default_setting");
+			DropItems = GetConfigBool("sod_dropitems");
+		}
+	}
 }
